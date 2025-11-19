53dc97015f0306a8c10418eadc97933823051cf2
elitsakutsarova <elitsa.biserova.kutsarova@student.howest.be>
2025-11-19 12:04:38 +0100

    changed base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://elitsakutsarova.github.io',
-    base: '/cck/',
+    base: '/deploy-astro/',
 });
